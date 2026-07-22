77efd1e335aefd8f6a1ba1ba38a73ef9b7900a10
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-07-22 11:11:11 -0300

    Adicionando variável 'sufixo' e saída correspondente no arquivo de outputs (Parte 4 da aula 2 do curso 'Terraform: gerenciar infraestrutura como código e modularizar recursos')

--- locals.tf
+++ locals.tf
@@ -1,3 +1,4 @@
 locals {
     nome_final_bucket = "${var.bucket_name}-${var.region}"
+    sufixo = var.region == "sa-east-1" ? "br": "global"
 }
--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "nome_do_bucket" {
 output "bucket_existente" {
     value = data.aws_s3_bucket.bucket_existente.arn
 }
+
+output "sufixo_escolhido" {
+    value = local.sufixo
+}
