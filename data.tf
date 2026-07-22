c0665bf73aba49e2b2dbe21cb8ae10026d3befde
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-07-22 10:50:19 -0300

    Adicionando dados e saída para o bucket S3 existente. (Parte 3 da aula 2 do curso 'Terraform: gerenciar infraestrutura como código e modularizar recursos')

---
+++ data.tf
@@ -0,0 +1,3 @@
+data "aws_s3_bucket" "bucket_existente" {
+    bucket = aws_s3_bucket.primeiro_bucket.bucket
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "nome_do_bucket" {
     value = aws_s3_bucket.primeiro_bucket.bucket
 }
+
+output "bucket_existente" {
+    value = data.aws_s3_bucket.bucket_existente.arn
+}
