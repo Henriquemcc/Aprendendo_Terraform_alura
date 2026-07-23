cce8df3af692f7776bec48377cbcd92856a912d9
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-07-23 19:07:01 -0300

    Corrigindo nome do bucket s3

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "primero-bucket-henriquemcc"
+    bucket = "meu-primeiro-bucket-henriquemcc-us-east-1"
     key = "Aprendendo_Terraform_alura/terraform.tfstate"
     region = "us-east1"
     encrypt = true
