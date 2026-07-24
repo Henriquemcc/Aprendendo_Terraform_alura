ad7fba17ac560d946c8bc92e2d77bcaa728bb45a
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-07-23 22:20:42 -0300

    Alterando chave do backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "backend-bucket-henriquemcc-1"
-    key = "Aprendendo_Terraform_alura/terraform.tfstate"
+    key = "terraform.tfstate"
     region = "us-east-1"
     encrypt = true
     dynamodb_table = "terraform-henriquemcc-1-locks"
